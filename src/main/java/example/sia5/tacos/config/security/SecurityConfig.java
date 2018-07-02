5960cab9dad76b6d703137631c3d174b8cb13458
1415209 <1415209@22HW005424.SOAM.TCS.com>
2018-07-02 15:40:40 -0500

    Update

--- src/main/java/example/sia5/tacos/config/security/SecurityConfig.java
+++ src/main/java/example/sia5/tacos/config/security/SecurityConfig.java
@@ -1,26 +1,40 @@
 package example.sia5.tacos.config.security;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	/*
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth)
-			throws Exception {
 
-		auth.jdbcAuthentication()
-				.dataSource(dataSource)
-				.usersByUsernameQuery(
-						"select username, password, enabled from Users "
-								+ "where username=?")
-				.authoritiesByUsernameQuery(
-						"select username, authority from UserAuthorities "
-								+ "where username=?")
-				.passwordEncoder(new StandardPasswordEncoder("53cr3t"));
+	@Autowired
+	DataSource dataSource;
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		auth.jdbcAuthentication().dataSource(dataSource)
+				.usersByUsernameQuery("select username, password, enabled from Users " + "where username=?")
+				.authoritiesByUsernameQuery("select username, authority from UserAuthorities " + "where username=?")
+				.passwordEncoder(encoder());
+
+		auth.userDetailsService(userDetailsService);
 	}
-	*/
+
+	@Bean
+	public StandardPasswordEncoder encoder() {
+		return new StandardPasswordEncoder("53cr3t");
+	}
+
 }
